2965fe2a0bea98e437739dcb7bee1365a84434fb
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 14:34:58 +0530

    problem7 solved

---
+++ 02_PRACTICE/02_LEVEL-2/Table.java
@@ -0,0 +1,10 @@
+public class Table {
+    public static void main(String[] args) {
+        int n = 11;
+        for(int i = 1; i<11; i++){
+            // System.out.println(i);
+            int tabResult = n*i;
+            System.out.println(n + "*" + i + "=" + tabResult);
+        }
+    }
+}

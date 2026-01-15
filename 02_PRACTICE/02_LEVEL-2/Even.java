5047f301c69e4634d66a8c42771c4db4dd8009e5
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 14:19:25 +0530

    problem3 solved

---
+++ 02_PRACTICE/02_LEVEL-2/Even.java
@@ -0,0 +1,9 @@
+public class Even {
+    public static void main(String[] args) {
+        int n = 20;
+        for(int i = 1; i<=n; i++){
+            if(i%2 == 0)
+                System.out.println(i);
+        }
+    }
+}

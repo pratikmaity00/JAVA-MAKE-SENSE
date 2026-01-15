f4170290ddcddda63d39290926f7d6c9c7bab80f
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 12:48:47 +0530

    problem1 solved

---
+++ 02_PRACTICE/01_LEVEL-1/Ve.java
@@ -0,0 +1,11 @@
+public class Ve {
+    public static void main(String[] args) {
+        double checkNum = -1.8;
+        if(checkNum == 0) 
+            System.out.println((checkNum) + " " + "is a equivalent to Zero.");
+        else if(checkNum < 0 )
+            System.out.println((checkNum) + " " + "is a Negative Number.");
+        else 
+            System.out.println((checkNum) + " " + "is a Positive Number.");
+    }
+}

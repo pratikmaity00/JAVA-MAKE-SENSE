43ab0d65435630843e2fab8276619e6fabe28fe2
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 13:00:22 +0530

    problem3 solved

---
+++ 02_PRACTICE/01_LEVEL-1/Max.java
@@ -0,0 +1,10 @@
+public class Max {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        if(n1>n2)
+            System.out.println((n1) + " " + "is maximum");
+        else
+            System.out.println((n2) + " " + "is maximum");
+    }
+}

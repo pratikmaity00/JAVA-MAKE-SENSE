b7873d7ad1e98ed97d5da5192c22ebfd9d954aaa
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 13:50:47 +0530

    problem9 solved

---
+++ 02_PRACTICE/01_LEVEL-1/Vote.java
@@ -0,0 +1,10 @@
+public class Vote {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if(age>=18)
+            System.out.println("Eligible for Vote.");
+        else
+            System.out.println("Not eligible for Vote.");
+    }
+}

68558a7946031ff1526cae2710205e94364dba53
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 14:20:25 +0530

    problem4 solved

---
+++ 02_PRACTICE/02_LEVEL-2/Odd.java
@@ -0,0 +1,9 @@
+public class Odd {
+    public static void main(String[] args) {
+        int n = 20;
+        for(int i = 1; i<=n; i++){
+            if(i%2 != 0)
+                System.out.println(i);
+        }
+    }
+}

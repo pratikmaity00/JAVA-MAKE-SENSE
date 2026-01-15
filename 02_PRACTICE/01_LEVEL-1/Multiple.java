728b9f34b00a53bee4450bf8faf68a65f938865c
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 14:00:32 +0530

    problem10 solved

---
+++ 02_PRACTICE/01_LEVEL-1/Multiple.java
@@ -0,0 +1,11 @@
+public class Multiple {
+    public static void main(String[] args) {
+        int num = 21;
+        int multiple = 3*7;
+
+        if(num%multiple == 0)
+            System.out.println(num + " is multiple of both 3 and 7.");
+        else
+            System.out.println(num + " is not multiple of both 3 and 7.");
+    }
+}

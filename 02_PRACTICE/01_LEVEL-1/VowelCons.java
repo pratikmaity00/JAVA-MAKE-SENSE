36dc7d3187db603f4d4b54fbed3ba6addf2d3b9c
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 13:36:44 +0530

    problem6 solved

---
+++ 02_PRACTICE/01_LEVEL-1/VowelCons.java
@@ -0,0 +1,10 @@
+public class VowelCons {
+    public static void main(String[] args) {
+        char alpahbet = 'Z';
+
+        if(alpahbet == 'a'  || alpahbet == 'e' || alpahbet == 'i' || alpahbet == 'o' || alpahbet == 'u' || alpahbet == 'A'  || alpahbet == 'E' || alpahbet == 'I' || alpahbet == 'O' || alpahbet == 'U')
+            System.out.println(alpahbet + " is a Vowel.");
+        else
+            System.out.println(alpahbet + " is a Consonant.");
+    }
+}

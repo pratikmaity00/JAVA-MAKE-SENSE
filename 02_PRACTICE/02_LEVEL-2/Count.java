76ce1cd688c07f5417debf564e2842187818e7f0
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 14:48:17 +0530

    problem8 solved

---
+++ 02_PRACTICE/02_LEVEL-2/Count.java
@@ -0,0 +1,15 @@
+public class Count {
+    public static void main(String[] args) {
+        int count = 0;
+        int n = 123490;
+        // int len = String.valueOf(num).length();
+        // System.out.println(len);
+
+        while(n != 0) {
+            n /= 10;
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}

fe2d223923bb7fd5944b85bbda42fd2a53d1a938
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 12:55:54 +0530

    problem2 solved

---
+++ 02_PRACTICE/01_LEVEL-1/EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 2;
+        if(n%2 == 0)
+            System.out.println((n) + " " + "is a Even number.");
+        else
+            System.out.println((n) + " " + "is a Odd number.");
+    }
+}

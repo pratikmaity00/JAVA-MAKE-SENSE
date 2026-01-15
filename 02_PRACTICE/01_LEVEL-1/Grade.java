774b30967a67b0edbb08c8cb573a9b1182508f06
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 13:48:34 +0530

    problem8 done

---
+++ 02_PRACTICE/01_LEVEL-1/Grade.java
@@ -0,0 +1,14 @@
+public class Grade {
+    public static void main(String[] args) {
+        double mark = 80;
+
+        if(mark>=90) 
+            System.out.println("A");
+        else if(mark>=75)
+            System.out.println("B");
+        else if(mark>=60)
+            System.out.println("C");
+        else
+            System.out.println("Fail");
+    }
+}

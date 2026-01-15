cfb790bfa73e8fc8e0c3d300829adb21a3194b11
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-15 15:04:10 +0530

    problem9 solved

--- 02_PRACTICE/02_LEVEL-2/Reverse.java
+++ 02_PRACTICE/02_LEVEL-2/Reverse.java
@@ -1,5 +1,11 @@
 public class Reverse {
     public static void main(String[] args) {
-        
+        int num = -201, rev = 0;
+        while(num != 0){
+            int rem = num%10;
+            rev = rev*10 + rem;
+            num /= 10;
+        }
+        System.out.println(rev);
     }
 }

80c107e2d4a2e751927f6d4abbf59b048bba18f0
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-18 20:19:17 +0530

    7 done

---
+++ 02_PRACTICE/03_LEVEL-3/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5,fact = 1;
+        for(int i = 0; i<n; i++) {
+            fact *= (n-i);
+        }
+        System.out.println("Factorial of " + n + " is " + fact);
+    }
+}

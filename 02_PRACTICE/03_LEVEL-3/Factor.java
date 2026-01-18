b63e635d758eee3402d2b1d394455709416c8362
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-18 20:28:22 +0530

    7 done

---
+++ 02_PRACTICE/03_LEVEL-3/Factor.java
@@ -0,0 +1,10 @@
+public class Factor {
+    public static void main(String[] args) {
+        int num = 12;
+        for(int i = 1; i<=num; i++){
+            if(num%i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}

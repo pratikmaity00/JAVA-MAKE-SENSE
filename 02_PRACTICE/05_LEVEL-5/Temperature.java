ed7dd08587df613c8dcf20149cba31c4f5393aed
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-20 21:01:01 +0530

    8 done

---
+++ 02_PRACTICE/05_LEVEL-5/Temperature.java
@@ -0,0 +1,11 @@
+public class Temperature {
+    public static void main(String[] args) {
+        // Given temperature in Celsius, convert to Fahrenheit using condition.
+
+        double celsius = 25;
+
+        double farenheit = ((celsius*9)/5) + 32;
+
+        System.out.println(farenheit);
+    }
+}

5bc553cd309bfe45c7caed0f142629b00b189e9c
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-23 09:23:14 +0530

    object done

--- 01_BASIC/09_OBJECT AND CLASS/Obj.java
+++ 01_BASIC/09_OBJECT AND CLASS/Obj.java
@@ -1,6 +1,12 @@
-
+class Add {
+    int x = 4;
+    public void add() {
+        System.out.println("Hello");
+    }
+}
 public class Obj {
     public static void main(String[] args) {
-        
+        Add myObj = new Add();
+        myObj.add();
     }
 }

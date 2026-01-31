63ff6790c3dc0b49a26aa12369de5ec9a140a93e
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-31 12:33:42 +0530

    for each loop

---
+++ 01_BASIC/12_ARRAY/forEach.java
@@ -0,0 +1,9 @@
+public class forEach {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+        for(int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}

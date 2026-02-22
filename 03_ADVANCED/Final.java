03f14e2f60a39a4e431a0e0dfb38360442eef4c1
pratikmaity00 <pratikmaity27910@gmail.com>
2026-02-22 21:33:55 +0530

    final done

---
+++ 03_ADVANCED/Final.java
@@ -0,0 +1,21 @@
+
+public class Final {
+    public static void main(String[] args) {
+        final class NotUse {
+            // not inherit
+        }
+        class F1 {
+            final void show() {
+                System.out.println("F1");
+            }
+        }
+        class F2 extends F1{
+            // void show() {
+            //     System.out.println("F1");
+            // }
+        }
+        final int pi = 3.14;
+        // pi = 21; // not initailize again
+        System.out.println(pi);
+    }
+}
